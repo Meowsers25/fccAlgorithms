9b932cba148787ee0ae46caa2f414f8eb38026b4
Christian Lamothe <christianlamothe@Christians-Air.home>
2017-05-15 09:52:31 -0400

    reworked algos

--- factorialize.js
+++ factorialize.js
@@ -1,10 +1,17 @@
 function factorialize(num) {
-  if (num === 0 || num === 1)
+  //need variable for thr for loop
+  var newNum = num;
+  //use === to compare with 0
+  if(newNum === 0) {
     return 1;
-  for(var i = num -1; i >= 1; i--) {
+  }
+  
+  else {
+  for(i = 1; i < newNum; i++) {
     num = num * i;
   }
   return num;
 }
-
+}
 factorialize(5);
+factorialize(20);
--- reverseastring.js
+++ reverseastring.js
@@ -1,7 +1,12 @@
 function reverseString(str) {
-  str = str.split("").reverse().join("");
+  //splits string into array
+  str = str.split("");
+  //reverses the array
+  str = str.reverse();
+  //joins array- use single quotes!
+  str = str.join('');
+  
   return str;
 }
 
 reverseString("hello");
-reverseString("Howdy");
