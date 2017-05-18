b68cbe826eab9a1c3a6fc184b90bf76484f4b794
Chris <c.lamothe25@gmail.com>
2017-05-18 17:22:50 -0400

    added titleCase

---
+++ titleCase.js
@@ -0,0 +1,32 @@
+function titleCase(str) {
+  
+  //put string to all lowercase
+  str = str.toLowerCase();
+  
+  //split string into an array *notice the space' '
+  str = str.split(' ');
+  
+  //declare 
+  var capLetter = '';
+  
+  //iterate through each word of array
+  for(i = 0; i < str.length; i++) {
+    
+    //split each item in array into individual characters
+    capLetter = str[i].split("");
+    
+    //capitalize first character of each value
+    capLetter[0] = capLetter[0].toUpperCase();
+    
+    //join the characters to reform the words
+    str[i] = capLetter.join("");
+  }
+  
+  //joins array back into the string and returns it
+  return str.join(" ");
+  
+}
+
+titleCase("HERE IS MY HANDLE HERE IS MY SPOUT");
+
+//WOO HOO!
