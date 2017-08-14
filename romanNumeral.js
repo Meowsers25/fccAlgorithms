b74d2cfd597a753221abe860df6c3917403ce0fa
Chris <c.lamothe25@gmail.com>
2017-08-14 15:44:40 -0400

    added romanNumeral.js

---
+++ romanNumeral.js
@@ -0,0 +1,14 @@
+function convert(num){
+    var roman = "";
+    var romanNumeral = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    var numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+
+    for(var i = 0; i < numbers.length; i++){
+        while(num>=numbers[i]){
+            roman = roman + romanNumeral[i];
+            num = num - numbers[i];
+        }
+    }
+    console.log(roman);
+}
+convert(536);
