d92ed3393e793416ecb5a77e7309cddd8901840d
Chris <c.lamothe25@gmail.com>
2017-09-27 22:50:43 -0400

    added booHoo.js

---
+++ booHoo.js
@@ -0,0 +1,11 @@
+function booWho(bool) {
+    // What is the new fad diet for ghost developers? The Boolean.
+
+    //typeof checks if it is a boolean
+    return typeof bool === 'boolean';
+
+    
+  }
+  
+  console.log(booWho(null));
+  
